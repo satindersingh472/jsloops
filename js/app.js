f7c7580896c5fe815e3876ba059149f303188b3c
satinder <satindersingh472@gmail.com>
2022-07-09 10:20:44 -0600

    finished knowledge check

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let number = 0;
+while ( number < 50){
+    console.log (number);
+    number = number + 1; 
+}
+ number = 0;
+while (number > -50){
+    console.log(number);
+    number = number - 1;
+}
+number = 0;
+while (number < 100){
+    console.log(number);
+    number = number + 2 ;
+}
